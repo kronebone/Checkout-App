db28f359c6b9600114c3e036d7434ed9042f8089
Alex Reichert <ajreichert87@gmail.com>
2019-07-24 20:09:20 -0400

    comments added
    better explanation of config parameters

--- checkout_config.py
+++ checkout_config.py
@@ -1,5 +1,5 @@
+# separate camera types with a comma and enclose in a single set of quotes
 camera_types = "put, all, camera, types, here"
 
+# paste disclaimer in triple quotes to allow for a more verbose disclaimer if desired
 disclaimer = """Put disclaimer here"""
-
-
